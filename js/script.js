eb6d80632455c317929829b1e989b9a5b06c7a69
Pickles03 <elizaveta.kris@gmail.com>
2025-03-20 19:00:27 +0100

    attempt 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
-fetch ('https://rickandmortyapi.com/api/character/?page=1')
+/* fetch ('https://rickandmortyapi.com/api/character/?page=1')
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => console.log(data)) */
+
+// api para los personasjes de rick and morty: https://rickandmortyapi.com/
+
+const characters = document.getElementById('character-list');
+
+fetch('https://rickandmortyapi.com/api/character/?page=1')
+.then((response) => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
+.then((data) => {
+    data.results.forEach((character) => {
+        const characterCard = document.createElement('div');
+        characterCard.classList.add('character-card');
+        characterCard.innerHTML = `
+        <img src="${character.image}" alt="${character.name}" />
+        <h3>${character.name}</h3>
+        <p>Status: ${character.status}</p>
+        <p>Species: ${character.species}</p>`;
+        characters.appendChild(characterCard);
+    });
+})
+.catch((error) => {
+    console.error('Error fetching data:', error);
+});
